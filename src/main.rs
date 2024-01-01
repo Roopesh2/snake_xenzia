b1e82ea9b7f96269ac093eec50cbb3c0b3a1a60f
Roopesh <62459820+Roopesh2@users.noreply.github.com>
2024-01-01 21:38:40 +0530

    made boundary

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 /**
-* plan
-* draw outline,
-* 
+* plan [x]
+* draw outline, [x]
+*
 *
 *
 */
 
-const SIZE: u8 = 30;
+const WIDTH: usize = 30;
+const HEIGHT: usize = 13;
+const WALL_HORZ: u8 = 1;
+const WALL_VERT: u8 = 2;
+const MAP_TILES: [&str; 5] = [" ", "―", "|", "=", "@"];
 fn main() {
-	draw_wall();
+    let mut map: [[u8; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
+    create_wall(&mut map);
+    print_map(&mut map);
 }
 
-fn draw_wall() {
-	for _ in 0..SIZE {
+fn print_map(map: &mut [[u8; WIDTH]; HEIGHT]) {
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            print!("{}", MAP_TILES[map[i][j] as usize]);
+        }
+        print!("\n");
+    }
+}
 
-	}
+fn create_wall(map: &mut [[u8; WIDTH]; HEIGHT]) {
+    for i in 0..WIDTH {
+        // top part
+        map[0][i] = WALL_HORZ;
+        // bottom part
+        map[HEIGHT - 1][i] = WALL_HORZ;
+    }
+    for i in 1..HEIGHT - 1 {
+        // top part
+        map[i][0] = WALL_VERT;
+        // bottom part
+        map[i][WIDTH - 1] = WALL_VERT;
+    }
 }
